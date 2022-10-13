583554b56e0855847fe5690d3fb4313b20aa8190
Prayagmayekar29 <prayagmayekar29@eng.rizvi.edu.in>
2022-10-13 23:41:54 +0530

    UPDATED FOLDER AND DID STYLING

--- home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from home.models import Product,order,ordered_items
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(order)
-admin.site.register(ordered_items)
